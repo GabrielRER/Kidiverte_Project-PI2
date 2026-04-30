bf3309e58e21da9663a4d51f213efe6b02e06a19
CamilaS-Git <cami.santos.b1712@gmail.com>
2026-04-29 23:10:57 -0300

    ADD CRUD

---
+++ src/app/app.module.ts
@@ -0,0 +1,10 @@
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  imports: [
+    FormsModule,
+    HttpClientModule
+  ]
+})
+export class AppModule {}
