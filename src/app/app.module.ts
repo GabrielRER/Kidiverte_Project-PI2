d07f3ef107c67fcaadc4785be17444bd1a94822a
Eduardo-Leite7 <duduleiteriveiro@gmail.com>
2026-05-20 22:00:26 -0300

    Footer Finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { Footer } from './Components/footer/footer';
+import { RouterModule } from '@angular/router';
+import { routes } from './app.routes';
 
 @NgModule({
   imports: [
     FormsModule,
+    Footer,
+    RouterModule.forRoot(routes),
     HttpClientModule
   ]
 })
