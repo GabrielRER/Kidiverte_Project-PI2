c02e5656067a73cac883fb1c0f34b31caa5b8965
GabrielRER <gabrielramonevangelistaramos@gmail.com>
2026-05-24 23:40:32 -0300

    wip: home-page

--- src/app/Components/shared/footer/footer.spec.ts
+++ src/app/Components/shared/footer/footer.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Footer } from './footer.ts';
+import { Footer } from './footer';
 
 describe('Footer', () => {
   let component: Footer;
