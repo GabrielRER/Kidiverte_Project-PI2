16809bfa5a6b364561166142a82d59c033fb491e
GabrielRER <gabrielramonevangelistaramos@gmail.com>
2026-05-26 00:00:48 -0300

    adeus

--- src/app/Components/header/header.ts
+++ src/app/Components/header/header.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
-import { Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-header',
+  standalone: true,
   imports: [],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {@Output() abrirCarrinho = new EventEmitter<void>();
+export class Header {
+  @Output() abrirCarrinho = new EventEmitter<void>();
 
-abrirCartSidebar() {
-  this.abrirCarrinho.emit();
-}}
+  abrirCartSidebar() {
+    console.log('Botão clicado');
+    this.abrirCarrinho.emit();
+  }
+}
