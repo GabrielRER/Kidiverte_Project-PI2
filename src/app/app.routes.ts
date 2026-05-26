78623c0f49d8d214f33313ddd5d249dd62af2e5a
CamilaS-Git <cami.santos.b1712@gmail.com>
2026-05-26 00:03:59 -0300

    add routes plp

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-<<<<<<< HEAD
 import { Home } from './Components/home/home';
 import { Card } from './Components/shared/card/card';
 import { ProductDetail } from './Components/product-detail/product-detail';
 import { Shopping } from './Components/shopping/shopping';
+import { PlpComponent } from './Components/plp/plp';
 
 export const routes: Routes = [
   { path: '', component: Home },
@@ -11,18 +11,8 @@ export const routes: Routes = [
   { path: 'card', component: Card },
   { path: 'produto/:id', component: ProductDetail },
   { path: 'carrinho', component: Shopping },
-=======
-import { PlpComponent } from './Components/plp/plp';
+  { path: '', component: PlpComponent},
+  {path: 'pdp/:id', loadComponent: () =>
+      import('./Components/plp/plp').then(m => m.PlpComponent)}
 
-export const routes: Routes = [
-  {
-    path: '',
-    component: PlpComponent
-  },
-  {
-    path: 'pdp/:id',
-    loadComponent: () =>
-      import('./Components/plp/plp').then(m => m.PlpComponent)
-  }
->>>>>>> PLP
-];
+]
