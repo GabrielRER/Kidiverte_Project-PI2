73dc6665068ba84b9017d06515b46b338b932eba
Eryka Marcal <erykamarcalt@gmail.com>
2026-05-26 17:53:14 -0300

    adiciona comentarios no código

--- src/app/components/cart-sidebar/cart-sidebar.ts
+++ src/app/components/cart-sidebar/cart-sidebar.ts
@@ -1,7 +1,7 @@
 import { Component, Output, EventEmitter, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { CartService } from '../../services/cart.service';
+import { CartService } from '../../services/cart.service'; // importa a lógica do carrinho
 
 @Component({
   selector: 'app-cart-sidebar',
@@ -11,15 +11,16 @@ import { CartService } from '../../services/cart.service';
   styleUrl: './cart-sidebar.css',
 })
 export class CartSidebar {
-  @Output() fechar = new EventEmitter<void>();
+  @Output() fechar = new EventEmitter<void>(); //cria um evento (EventEmitter) chamado fechar e envia o aviso ao componente pai (outPut)
 
-  cartService: CartService = inject(CartService);
+  cartService: CartService = inject(CartService); // da acesso ao cartService 
 
-  fecharSidebar() {
-    this.fechar.emit();
+  fecharSidebar() {  
+    this.fechar.emit(); // envia o evento com o emit.
   }
 
   formatPrice(value: number): string {
     return value.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    // define como o valor deve aparecer 
   }
 }
